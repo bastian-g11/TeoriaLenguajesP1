a750c7899e0f76a29b5599281019748c28c6b037
Sebastián Gómez <sebastian.gomezr11@gmail.com>
2020-10-19 22:39:10 -0500

    Integración de Asset para abrir File y primer intento de separar líneas

---
+++ Assets/Scripts/TextReader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TextReader : MonoBehaviour
+{
+    public Text txt;
+    public string lineaTexto;
+    public static TextReader instance;
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText()
+    {
+        lineaTexto = txt.text;
+        Debug.Log(lineaTexto);
+        Recorrer();
+    }
+    
+    public void Recorrer()
+    {
+        //Una línea
+        string aLine = null;
+        //Todas las líneas
+        string aParagraph = null;
+        System.IO.StringReader strReader = new StringReader(lineaTexto);
+        while (true)
+        {
+            //Captua una línea
+            aLine = strReader.ReadLine();
+            if (aLine != null)
+            {
+                aParagraph = aParagraph + aLine + " ";
+                //Autómata Estructura
+                Debug.Log("Linea: " + aLine);
+            }
+            else
+            {
+                aParagraph = aParagraph + "\n";
+                break;
+            }
+        }
+    }
+}
